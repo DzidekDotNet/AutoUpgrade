4111b7df50914890e34a7887146d88c9b5eff32c
Tomasz Dzidek <t.dzidek@integra.pl>
2023-01-24 12:32:50 +0100

    Remove unnecessary files

--- AutoUpgradeService.Service.Library/IAutoUpgradeService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace AutoUpgradeService.Service.Library;
-
-public interface IAutoUpgradeService
-{
-    string GetVersion();
-    Task Upgrade(IFormFile newLibraryVersion, HttpContext httpContext);
-}
